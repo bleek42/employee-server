9f3c4ac7d2c6a2276a71dc0b80dfe5d84b577e92
bleek42 <brandonleek42@gmail.com>
2020-08-02 16:56:06 -0400

    fixed router

--- src/routes/employee-route.js
+++ src/routes/employee-route.js
@@ -4,17 +4,21 @@ const logger = require('./middleware/logger');
 const EmployeeService = require('./employee-service');
 
 const employeeRouter = express.Router();
+employeeRouter.use(express.json());
 
-employeeRouter.route('/employee').get(async (req, res, next) => {
+employeeRouter.get('/employee', async (req, res, next) => {
   try {
-    const employees = await EmployeeService.getAll();
+    const employees = await EmployeeService.getAll(req.app.get('db'));
     if (!employees) {
       res.status(400).json({
         error: 'Cannot GET employees!',
       });
-      return res.status(200).json({});
+      res.status(200).json({ employees });
+      next();
     }
   } catch (err) {
-    console.error(err);
+    next(err);
   }
 });
+
+module.exports = employeeRouter;
